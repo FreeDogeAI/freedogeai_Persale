b266297228b5becb884b3b4004e91a8cc36d9e91
FreeDogeAI <ft438554@gmail.com>
2025-05-09 14:51:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -143,6 +143,41 @@ window.addEventListener('DOMContentLoaded', () => {
     connectWallet();
   }
 });
+// Language switcher
+document.getElementById("languageSwitcher").addEventListener("change", function () {
+  const selectedLang = this.value;
+  const t = translations[selectedLang];
+  if (!t) return;
+
+  // Başlıklar
+  document.querySelector("h1").innerText = t.headline;
+  document.querySelector(".highlight-message").innerText = t.subtitle;
+
+  // Cüzdan butonu ve bilgileri
+  document.getElementById("connectWalletBtn").innerText = t.connect;
+  document.querySelector("h3").innerText = t.walletTitle;
+  document.querySelector("label[for='bnbAmount']").innerHTML = "<strong>" + t.bnbAmount + "</strong>";
+  document.getElementById("calculationResult").innerHTML = "<strong>" + t.receive + "</strong> <span id='fdaiAmount'>0</span> FDAI";
+  document.getElementById("buyBtn").innerText = t.buy;
+
+  // Bilgilendirme kutusu
+  const infoBox = document.querySelector(".info-box");
+  infoBox.querySelector("h3").innerText = t.info;
+  const ps = infoBox.querySelectorAll("p");
+  ps[0].innerText = t.note1;
+  ps[1].innerText = t.note2;
+  ps[2].innerText = t.note3;
+  ps[3].innerHTML = "<strong>NOTE:</strong> " + t.note4;
+
+  // Hakkında ve Topluluk
+  document.querySelector("h3:nth-of-type(2)").innerText = t.about;
+  document.querySelector(".wallet-info").nextSibling.textContent = t.aboutText;
+  document.querySelector("a[href$='Whitepaper.pdf']").innerText = t.download;
+  document.querySelector("h3:nth-of-type(3)").innerText = t.community;
+  const links = document.querySelectorAll("ul li a");
+  links[0].innerText = t.telegram + ": @freedogeaiFDAI";
+  links[1].innerText = t.twitter + ": @FreeDogeAI_FDAI";
+});
 
 // Wallet connection handler
 async function connectWallet() {
